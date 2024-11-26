12ec7cca067a6bf3c9093602cf374ab7c35642e7
DmitryLV <dml-vip@mail.ru>
2024-11-26 23:18:05 +0100

    learn variable
    1 learn variable

---
+++ CSLight/CSLight/Program001Variable.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSLight
+{
+    internal class Program001Variable
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+            // variable
+            // declaration and initialization
+            // definition, (type + name) this is a variable declaration,
+            // the value of a variable is initialization or assignment
+            // type + name = value
+
+
+
+            // the main type
+            // Integer types
+            byte b;
+            sbyte sb;
+            short s;
+            ushort us;
+            int i;
+            uint ui;
+            long l;
+            ulong ul;
+
+
+            byte b2; // 0 до 255
+            sbyte sb2; // -128 - 127
+            short s2; // -32 768 - 32 767
+            ushort us2; // 0 до 65 535
+            int i2; // -2 147 483 648 - 2 147 483 647
+                    // int (basic type)
+            uint ui2; // 0 - 4 294 672 295
+            long l2 = long.MaxValue; // 
+            long l3 = long.MinValue; // 
+            ulong ul2; //
+
+            float f; // up to about 7 decimal places
+            double d; // up to about 15 decimal places
+
+            float f2 = 5.7f; // float < double 
+            double d2 = 5.7; // 
+
+
+
+            // haracter type
+            char c = ' ';
+
+            // string type (basic type)
+            string str = "string Hello, World! Hou are you";
+            // string is a set of chars
+
+
+
+            // logical type
+            // boolean variable or bool (basic type)
+            // it takes the values true or false
+
+
+            bool bl;
+
+
+            // basic type
+            // int 
+            // float
+            // string
+            // bool
+
+
+            // name for the variable
+            // it should be understandable for others
+            // the name conveys the essence of the variable itself
+            // in English
+            // in camelCase style, the beginning is capitalized and the next word is capitalized
+            // there are no underscores, everything is merged
+            // the name of the variable reflects the essence of what it will store in itself
+
+        }
+    }
+}
